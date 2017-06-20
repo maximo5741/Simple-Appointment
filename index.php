ef881be50d4b1ba161ba076367521994c13c30a6
Maximo <maximo.delvalle001@mymdc.net>
2017-06-20 02:27:05 -0400

    This is the finish version without any filering. It only has access to a database.

--- index.php
+++ index.php
@@ -1,9 +1,31 @@
+<?php
+$connect_appointment = new mysqli("localhost","root","elmastergotey32","appointment");
+
+if($connect_appointment->connect_errno){
+    die("connection error:" . $connect_appointment->connect-errno );
+}
+
+if($_POST["submit"]){
+
+$errno = CUBRID_AUTOCOMMIT_TRUE;
+$fullname = $_POST["fullname"];
+$phone = $_POST["phonenumeber"];
+$email = $_POST["email"];
+$date = $_POST["date"];
+
+    if($errno){
+        $connect_appointment->query("INSERT INTO `cdata` (`fullname`, `phone`, `email`, `date`, `timestamp`) VALUES ( '".$fullname."', '".$phone."', '".$email."', '".$date."', CURRENT_TIMESTAMP);");
+
+    }
+
+}
+?>
 <html>
     <head>
         <link rel = "stylesheet" type = "text/css" href = "main.css" />
     </head>
     <body>
-        <form class="form-container">
+        <form class="form-container" method="post" action="<?php $_SERVER["PHP_SELF"]; ?>">
         <div class="form-title"><h2>Appointment</h2></div>
         <div class="form-title">Full Name</div>
         <input class="form-field" type="text" name="fullname" required /><br />
@@ -20,8 +42,11 @@
         <input class="form-field" type="date" name="date" required /><br />
         
         <div class="submit-container">
-        <input class="submit-button" type="submit" value="Submit" />
+        <input class="submit-button" name="submit" type="submit" value="Submit" />
         </div>
         
     </body>
 </html>
+<script>
+
+</script>
