2dcae05863e059770ef313df7b15befbb2c99010
Maximo <maximo.delvalle001@mymdc.net>
2017-06-20 16:51:53 -0400

    Working version with date and phone number authentication

--- index.php
+++ index.php
@@ -15,13 +15,15 @@ $date = $_POST["date"];
 
     if($errno){
         $connect_appointment->query("INSERT INTO `cdata` (`fullname`, `phone`, `email`, `date`, `timestamp`) VALUES ( '".$fullname."', '".$phone."', '".$email."', '".$date."', CURRENT_TIMESTAMP);");
-
     }
-
 }
 ?>
+
 <html>
     <head>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script type="text/javascript" src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+        <script src="//cdn.jsdelivr.net/webshim/1.14.5/polyfiller.js"></script>
         <link rel = "stylesheet" type = "text/css" href = "main.css" />
     </head>
     <body>
@@ -30,23 +32,90 @@ $date = $_POST["date"];
         <div class="form-title">Full Name</div>
         <input class="form-field" type="text" name="fullname" required /><br />
         <div class="form-title">Phone Number</div>
-        <input class="form-field" type="text" placeholder="(555) 555-5555" name="phonenumeber" required /><br />
-        <div class="form-title">Email</div>
-        <input class="form-field" type="email" placeholder="example@email.com" name="email" required /><br />
-        <!--<div class="form-title">Place</div>
-        <select class="form-field" name="place" required>
-          <option value="place 1">place 1</option>
-          <option value="place 2">place 2</option>
-        </select>-->
-        <div class="form-title">Date</div>
-        <input class="form-field" type="date" name="date" required /><br />
-        
-        <div class="submit-container">
-        <input class="submit-button" name="submit" type="submit" value="Submit" />
-        </div>
-        
-    </body>
-</html>
-<script>
+        <input class="form-field" type="text" placeholder="(555) 555-5555" maxlength="14" name="phonenumeber" id="phone-number" required /><br />
+            <!-- <input placeholder="Enter phone number" class="input-phone" type="tel"><br />-->
+         <div class="form-title">Email</div>
+         <input class="form-field" type="email" placeholder="example@email.com" name="email" required /><br />
+         <div class="form-title">Date</div>
+         <input class="form-field" type="date" name="date" required /><br />
 
-</script>
+         <div class="submit-container">
+         <input class="submit-button" name="submit" type="submit" value="Submit" />
+         </div>
+
+     </body>
+ </html>
+
+
+ <script>
+
+
+     webshims.setOptions('forms-ext', {types: 'date'});
+     webshims.polyfill('forms forms-ext');
+     $.webshims.formcfg = {
+         en: {
+             dFormat: '-',
+             dateSigns: '-',
+             patterns: {
+                 d: "mm-dd-yy"
+             }
+         }
+     };
+
+     $('#phone-number')
+
+         .keydown(function (e) {
+             var key = e.which || e.charCode || e.keyCode || 0;
+             $phone = $(this);
+
+             // Don't let them remove the starting '('
+             if ($phone.val().length === 1 && (key === 8 || key === 46)) {
+                 $phone.val('(');
+                 return false;
+             }
+             // Reset if they highlight and type over first char.
+             else if ($phone.val().charAt(0) !== '(') {
+                 $phone.val('('+String.fromCharCode(e.keyCode)+'');
+             }
+
+             // Auto-format- do not expose the mask as the user begins to type
+             if (key !== 8 && key !== 9) {
+                 if ($phone.val().length === 4) {
+                     $phone.val($phone.val() + ')');
+                 }
+                 if ($phone.val().length === 5) {
+                     $phone.val($phone.val() + ' ');
+                 }
+                 if ($phone.val().length === 9) {
+                     $phone.val($phone.val() + '-');
+                 }
+             }
+
+             // Allow numeric (and tab, backspace, delete) keys only
+             return (key == 8 ||
+             key == 9 ||
+             key == 46 ||
+             (key >= 48 && key <= 57) ||
+             (key >= 96 && key <= 105));
+         })
+
+         .bind('focus click', function () {
+             $phone = $(this);
+
+             if ($phone.val().length === 0) {
+                 $phone.val('(');
+             }
+             else {
+                 var val = $phone.val();
+                 $phone.val('').val(val); // Ensure cursor remains at the end
+             }
+         })
+
+         .blur(function () {
+             $phone = $(this);
+
+             if ($phone.val() === '(') {
+                 $phone.val('');
+             }
+         });
+ </script>
